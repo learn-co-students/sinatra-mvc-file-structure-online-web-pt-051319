a8c2d1011391fee1aab0655ba4239ea9c373585d
scratchglory <adellakenn@gmail.com>
2019-08-06 08:16:12 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
+# The C component
 class ApplicationController < Sinatra::Base
+	configure do
+		set :views, 'app/views'
+		set :public_dir, 'public'
+	end
 
-  configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
-  end
-
-  get "/" do
-  	erb :index
-  end
+	get '/' do
+		erb :index
+	end
 end
